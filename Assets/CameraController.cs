b1e2b03dc85ad442209312d07dfe38476cc22dff
JohnKontolatos <dodeardi@gmail.com>
2018-12-10 19:30:37 +0700

    Initial
    First

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    float Xmin;
+    [SerializeField]
+    float Xmax;
+    [SerializeField]
+    float Ymin;
+    [SerializeField]
+    float Ymax;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, Xmin, Xmax),Mathf.Clamp(target.position.y, Ymin, Ymax),transform.position.z);
+    }
+
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 10f;
+    [SerializeField]
+    float height = 30f;
+
+    float jump;
+
+    bool isJump;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y + jump);
+
+        if (Input.GetKeyDown("space") && isJump)
+            jump += height;
+        else
+            jump = 0.0f;
+
+        rb.position.Normalize();
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            Debug.Log("bisa");
+            isJump = true;
+        }
+        
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag=="ground")
+            isJump = false;
+        Debug.Log("gakbisa");
+    }
+}
